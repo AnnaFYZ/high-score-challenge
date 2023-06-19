2dd3b61568ce3f1ffd6a290973c0357d1307df26
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-06-19 22:09:42 +0100

    level3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
+import "./App.css";
+import HighScoreTables from "./High-score-tables";
 
-import './App.css';
 
-
-function App(props) {
-  let allCountryScores = props.countryScore;
-  return (
-    <div className="App">
-      <h1>High Scores per Country</h1>
-     { allCountryScores.map(country => {
-      return (
-        <div className="CountryCard">
-          <h3>HIGH SCORES: {country.name}</h3>
-          <div className="CardBorder">
-            {country.scores.map(person => {
-              return (
-                <div className="ScoreLine">
-                  <p className="Name">{person.n}</p>
-                  <p className="Score">{person.s}</p>
-                </div>
-              );
-            })}
-            
-          </div>
-        </div>
-      );
+function App() {
+        <HighScoreTables />;
      }
-      )}
-      
-    </div>
-  );
-}
 
 export default App;
